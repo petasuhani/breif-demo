77603912cac64907294e4e287b6065d063ddc41d
petasuhani <petasuhani@gmail.com>
2026-06-06 13:35:31 +0530

    Added python basics

---
+++ Python_-basics.py
@@ -0,0 +1,80 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMbq9NsHZqTl4co8jTj7Y/M",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/petasuhani/breif-demo/blob/main/Python_-basics.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "print(\"My name is paupi\")"
+      ],
+      "metadata": {
+        "id": "mnQ2O4jC7fW2",
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "outputId": "6d033d3e-6c88-47c1-8cbc-efd17ca8d4a0"
+      },
+      "execution_count": 3,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "My name is paupi\n"
+          ]
+        }
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "name=\"lucky\"\n",
+        "age=18\n",
+        "print(name)\n",
+        "print(age)"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "AKOgpaeO86I-",
+        "outputId": "425e611d-871b-43e5-f1b7-9b4b6e2ccd79"
+      },
+      "execution_count": 4,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "lucky\n",
+            "18\n"
+          ]
+        }
+      ]
+    }
+  ]
+}
